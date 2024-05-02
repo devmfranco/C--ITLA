08fc88ec6ae89f513d0b4ce90ec51a83734cf137
devmfranco <108529460+devmfranco@users.noreply.github.com>
2024-05-02 14:18:36 -0400

    ..

--- Semana 1/sumaProducto/obj/Debug/net8.0/sumaProducto.AssemblyInfo.cs
+++ Semana 1/sumaProducto/obj/Debug/net8.0/sumaProducto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sumaProducto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7937a024a90f01acde8c2836209f7f9d00e9c4c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("sumaProducto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sumaProducto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
