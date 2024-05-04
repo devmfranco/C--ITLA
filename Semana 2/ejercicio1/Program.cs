46c03befef3ff1faac90c27a6c4be545c8c57461
devmfranco <202001983@p.uapa.edu.do>
2024-05-04 07:21:29 -0700

    update

--- Semana 2/ejercicio1/Program.cs
+++ Semana 2/ejercicio1/Program.cs
@@ -1,2 +1,36 @@
 ﻿using System;
 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresar el primer numero: ");
+        double primerNumero = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingresar el segundo numero: ");
+        double segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+        if (primerNumero > segundoNumero)
+        {
+            double suma = primerNumero + segundoNumero;
+            double resta = primerNumero - segundoNumero;
+            Console.WriteLine("La suma es: " + suma);
+            Console.WriteLine("La diferencia es: " + resta);
+        }
+        else
+        {
+            double producto = primerNumero * segundoNumero;
+            if (primerNumero != 0)  
+            {
+                double division = primerNumero / segundoNumero;
+                Console.WriteLine("El producto es: " + producto);
+                Console.WriteLine("La división es: " + division);
+            }
+            else
+            {
+                Console.WriteLine("No es posible dividir entre cero");
+            }
+        }
+    }
+}
