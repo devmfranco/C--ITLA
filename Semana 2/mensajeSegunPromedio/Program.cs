afa67a1e4a14f9ab470f7d30398ac9658b868c52
devmfranco <202001983@p.uapa.edu.do>
2024-05-04 22:04:00 -0400

    Update second weekend

---
+++ Semana 2/mensajeSegunPromedio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Escriba primera nota: ");
+        double primeraNota = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Escriba  segunda nota: ");
+        double segundaNota = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Escriba tercera nota: ");
+        double terceraNota = Convert.ToDouble(Console.ReadLine());
+
+        double promedio = (primeraNota + segundaNota + terceraNota) / 3;
+
+        if (promedio >= 7)
+        {
+            Console.WriteLine("Promocionado");
+        }
+        else if (promedio >= 4 && promedio < 7)
+        {
+            Console.WriteLine("Regular");
+        }
+        else
+        {
+            Console.WriteLine("Reprobado");
+        }
+    }
+}
