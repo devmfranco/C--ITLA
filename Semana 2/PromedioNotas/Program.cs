d46d0a2a0671ce0af66b7bc6ea03651dc544aa11
devmfranco <202001983@p.uapa.edu.do>
2024-05-04 21:52:28 -0400

    Cambios

---
+++ Semana 2/PromedioNotas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese la primera nota: ");
+        double primeraNota = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la segunda nota: ");
+        double segundaNota = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingrese la tercera nota: ");
+        double terceraNota = Convert.ToDouble(Console.ReadLine());
+
+        double promedio = (primeraNota + segundaNota + terceraNota / 3);
+
+        if (promedio >= 7)
+        {
+            Console.WriteLine("Promocionado");
+        }
+
+        Console.WriteLine();
+    }
+}
