b18417d1e8fb4d9ed7c423007e38f5f4056fe5ac
devmfranco <202001983@p.uapa.edu.do>
2024-05-11 15:57:48 -0400

    ...

---
+++ Semana 3/sumaPromedio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+     
+        Console.WriteLine("Ingresa cuatro valores numéricos:");
+
+        Console.Write("Primer valor: ");
+        double primervalor = double.Parse(Console.ReadLine());
+
+        Console. Write("Segundo valor: ");
+        double segundoValor= double.Parse(Console.ReadLine());
+
+        Console.Write("Tercer valor: ");
+        double tercerValor = double.Parse(Console.ReadLine());
+
+        Console.Write("Cuarto valor: ");
+        double cuartoValor = double.Parse(Console.ReadLine());
+
+      
+        double suma = primervalor + segundoValor + tercerValor + cuartoValor;
+
+       
+        double promedio = suma / 4;
+
+        Console.WriteLine($"La suma de los cuatro valores es: {suma}");
+        Console.WriteLine($"El promedio de los cuatro valores es: {promedio}");
+    }
+}
